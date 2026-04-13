ac1ba730f0ef9ea0d69b3085d6792439deb796d6
williambale7 <nqc1842005@gmail.com>
2026-04-13 14:26:46 +0700

    update

---
+++ rtl/mac_unit/mac_ctrl.v
@@ -0,0 +1,73 @@
+module SystolicController (
+    input wire clk,
+    input wire reset,
+    input wire start,
+
+    output reg clear,
+    output reg valid,
+    output reg done,
+    output reg [3:0] cycle
+);
+    reg [1:0] state;
+
+    localparam IDLE  = 2'd0;
+    localparam CLEAR = 2'd1;
+    localparam RUN   = 2'd2;
+    localparam DONE  = 2'd3;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            state <= IDLE;
+            cycle <= 0;
+        end else begin
+            case (state)
+                IDLE: begin
+                    cycle <= 0;
+                    if (start)
+                        state <= CLEAR;
+                end
+
+                CLEAR: begin
+                    cycle <= 0;  
+                    state <= RUN;     
+                end
+
+                RUN: begin
+                    cycle <= cycle + 1;
+
+                    if (cycle == 7)
+                        state <= DONE;
+                end
+
+                DONE: begin
+                    state <= IDLE;
+                end
+            endcase
+        end
+    end
+
+    always @(*) begin
+        clear = 0;
+        valid = 0;
+        done  = 0;
+
+        case (state)
+            IDLE: begin
+                clear = 1;
+            end
+
+            CLEAR: begin
+                clear = 1;  
+            end
+
+            RUN: begin
+                valid = 1;   
+            end
+
+            DONE: begin
+                done = 1;
+            end
+        endcase
+    end
+
+endmodule
