3786467f02a7b5c087e16eb4590069641262cb41
williambale7 <nqc1842005@gmail.com>
2026-04-14 11:01:14 +0700

    update

--- rtl/npu.v
+++ rtl/npu.v
@@ -1,8 +1,11 @@
-module NPU (clk, reset, start, IFM, WGT, done);
+module NPU (clk, reset, start, IFM, WGT, done, c1_out, c2_out, c3_out, c4_out, c5_out, c6_out, c7_out, c8_out, c9_out);
     input clk, reset, start;
     input [215:0] IFM;
     input [215:0] WGT;
     output done;
+    output [47:0] c1_out, c2_out, c3_out; 
+    output [47:0] c4_out, c5_out, c6_out;
+    output [47:0] c7_out, c8_out, c9_out;
 
     wire [23:0] a1, a2, a3;
     wire [23:0] b1, b2, b3;
@@ -10,13 +13,43 @@ module NPU (clk, reset, start, IFM, WGT, done);
     wire [23:0] a1_mac, a2_mac, a3_mac;
     wire [23:0] b1_mac, b2_mac, b3_mac;
 
+    wire [47:0] c1_mac, c2_mac, c3_mac;
+    wire [47:0] c4_mac, c5_mac, c6_mac;
+    wire [47:0] c7_mac, c8_mac, c9_mac;
+
     wire done_input_buffer, done_mac;
 
     systolic_input_buffer input_buffer (
-    
+        .clk(clk),
+        .reset(reset),
+        .start(start),
+        .IFM(IFM),
+        .WGT(WGT),
+        .a1(a1), .a2(a2), .a3(a3),
+        .b1(b1), .b2(b2), .b3(b3),
+        .done(done_input_buffer)
     );
 
     mac_top mac (
-    
+        .clk(clk),
+        .reset(reset),
+        .start(start),
+        .a1(a1_mac), .a2(a2_mac), .a3(a3_mac),
+        .b1(b1_mac), .b2(b2_mac), .b3(b3_mac),
+        .c1(c1_mac), .c2(c2_mac), .c3(c3_mac),
+        .c4(c4_mac), .c5(c5_mac), .c6(c6_mac),
+        .c7(c7_mac), .c8(c8_mac), .c9(c9_mac),
+        .done(done_mac)
     );
+
+    assign done = done_input_buffer & done_mac;
+    assign c1_out = c1_mac;
+    assign c2_out = c2_mac;
+    assign c3_out = c3_mac;
+    assign c4_out = c4_mac;
+    assign c5_out = c5_mac;
+    assign c6_out = c6_mac;
+    assign c7_out = c7_mac;
+    assign c8_out = c8_mac;
+    assign c9_out = c9_mac;
 endmodule
---
+++ testbench/npu_tb.v
@@ -0,0 +1,48 @@
+module tb ();
+    reg clk, reset, start;
+    reg [215:0] IFM, WGT;
+    wire done;
+    wire [23:0] c1_out, c2_out, c3_out;
+    wire [23:0] c4_out, c5_out, c6_out; 
+    wire [23:0] c7_out, c8_out, c9_out;
+
+    NPU npu (
+        .clk(clk),
+        .reset(reset),
+        .start(start),
+        .IFM(IFM),
+        .WGT(WGT),
+        .done(done),
+        .c1_out(c1_out), .c2_out(c2_out), .c3_out(c3_out),
+        .c4_out(c4_out), .c5_out(c5_out), .c6_out(c6_out),
+        .c7_out(c7_out), .c8_out(c8_out), .c9_out(c9_out)
+    );
+
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0;
+        reset = 1;
+        start = 0;
+        IFM = {
+            24'd1, 24'd2, 24'd3,
+            24'd4, 24'd5, 24'd6,
+            24'd7, 24'd8, 24'd9
+        };
+        WGT = {
+            24'd1, 24'd2, 24'd3,
+            24'd4, 24'd5, 24'd6,
+            24'd7, 24'd8, 24'd9
+        };
+        #10;
+        reset = 0; 
+        start = 1;
+        #10;
+        start = 0;
+        #1000 $finish;
+    end
+
+    initial begin
+        $dumpfile("npu_tb.vcd");
+        $dumpvars();
+    end
+endmodule
