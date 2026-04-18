2c4c06d9a92e877b5fcac0b323b6f37ea34f0556
williambale7 <nqc1842005@gmail.com>
2026-04-18 19:59:42 +0700

    update

--- testbench/tb_cut_bit.v
+++ testbench/tb_cut_bit.v
@@ -2,16 +2,16 @@
 
 module tb ();
     reg clk, reset, enable;
-    reg signed [47:0] din;
-    wire signed [23:0] dout;
+    reg signed [47:0] din_0, din_1, din_2, din_3, din_4, din_5, din_6, din_7, din_8;
+    wire signed [23:0] dout_0, dout_1, dout_2, dout_3, dout_4, dout_5, dout_6, dout_7, dout_8;
     wire done;
 
-    Q16_32_to_Q8_16_pipe u_dut (
-        .clk(clk),
-        .reset(reset),
-        .enable(enable),
-        .din(din),
-        .dout(dout),
+    Q16_32_to_Q8_16_pipe u_pipe (
+        .clk(clk), .reset(reset), .enable(enable),
+        .din_0(din_0), .din_1(din_1), .din_2(din_2), .din_3(din_3), .din_4(din_4),
+        .din_5(din_5), .din_6(din_6), .din_7(din_7), .din_8(din_8),
+        .dout_0(dout_0), .dout_1(dout_1), .dout_2(dout_2), .dout_3(dout_3), .dout_4(dout_4),
+        .dout_5(dout_5), .dout_6(dout_6), .dout_7(dout_7), .dout_8(dout_8),
         .done(done)
     );
 
@@ -21,11 +21,17 @@ module tb ();
         clk = 0;
         reset = 1;
         enable = 0;
-        din = 0;
-
         #10 reset = 0;
 
-        din = 48'sd4026531840; 
+        din_0 = 48'sd4026531840;
+        din_1 = 48'sd4026531840;
+        din_2 = 48'sd4026531840;
+        din_3 = 48'sd4026531840;
+        din_4 = 48'sd4026531840;
+        din_5 = 48'sd4026531840;
+        din_6 = 48'sd4026531840;
+        din_7 = 48'sd4026531840;
+        din_8 = 48'sd4026531840;
         enable = 1;
         #10 enable = 0;
         #100; 
