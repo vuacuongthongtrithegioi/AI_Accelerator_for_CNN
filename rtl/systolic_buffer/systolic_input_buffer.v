a8a60b0ac2d68689b5bc139d4b7c9aac0d60bc9c
williambale7 <nqc1842005@gmail.com>
2026-04-24 16:50:59 +0700

    update

--- rtl/systolic_buffer/systolic_input_buffer.v
+++ rtl/systolic_buffer/systolic_input_buffer.v
@@ -1,39 +1,39 @@
 module systolic_input_top #(parameter DW=24)(
     input clk, reset, start,
 
-    input [215:0] IFM,
-    input [215:0] WGT,
+    input signed [215:0] IFM,
+    input signed [215:0] WGT,
 
-    output [DW-1:0] a1,a2,a3,
-    output [DW-1:0] b1,b2,b3,
+    output signed [DW-1:0] a1,a2,a3,
+    output signed [DW-1:0] b1,b2,b3,
     output done
 );
     wire [2:0] cycle;
     wire load;
 
-    wire [23:0] A00 = IFM[215:192];
-    wire [23:0] A01 = IFM[191:168];
-    wire [23:0] A02 = IFM[167:144];
+    wire signed [23:0] A00 = IFM[215:192];
+    wire signed [23:0] A01 = IFM[191:168];
+    wire signed [23:0] A02 = IFM[167:144];
 
-    wire [23:0] A10 = IFM[143:120];
-    wire [23:0] A11 = IFM[119:96];
-    wire [23:0] A12 = IFM[95:72];
+    wire signed [23:0] A10 = IFM[143:120];
+    wire signed [23:0] A11 = IFM[119:96];
+    wire signed [23:0] A12 = IFM[95:72];
 
-    wire [23:0] A20 = IFM[71:48];
-    wire [23:0] A21 = IFM[47:24];
-    wire [23:0] A22 = IFM[23:0];
+    wire signed [23:0] A20 = IFM[71:48];
+    wire signed [23:0] A21 = IFM[47:24];
+    wire signed [23:0] A22 = IFM[23:0];
 
-    wire [23:0] B00 = WGT[215:192];
-    wire [23:0] B01 = WGT[191:168];
-    wire [23:0] B02 = WGT[167:144];
+    wire signed [23:0] B00 = WGT[215:192];
+    wire signed [23:0] B01 = WGT[191:168];
+    wire signed [23:0] B02 = WGT[167:144];
 
-    wire [23:0] B10 = WGT[143:120];
-    wire [23:0] B11 = WGT[119:96];
-    wire [23:0] B12 = WGT[95:72];
+    wire signed [23:0] B10 = WGT[143:120];
+    wire signed [23:0] B11 = WGT[119:96];
+    wire signed [23:0] B12 = WGT[95:72];
 
-    wire [23:0] B20 = WGT[71:48];
-    wire [23:0] B21 = WGT[47:24];
-    wire [23:0] B22 = WGT[23:0];
+    wire signed [23:0] B20 = WGT[71:48];
+    wire signed [23:0] B21 = WGT[47:24];
+    wire signed [23:0] B22 = WGT[23:0];
 
     systolic_input_buffer_ctl ctrl (
         .clk(clk),
