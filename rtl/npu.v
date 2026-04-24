3608e84ec4a2fc35712fd3ff70fd3e82bbb6132a
williambale7 <nqc1842005@gmail.com>
2026-04-24 15:23:24 +0700

    update

--- rtl/npu.v
+++ rtl/npu.v
@@ -1,21 +1,35 @@
-module NPU (clk, reset, start, IFM, WGT, done, c1_out, c2_out, c3_out, c4_out, c5_out, c6_out, c7_out, c8_out, c9_out, done_input_buffer, done_mac);
+module NPU (clk, reset, start, 
+    IFM, WGT, done, 
+    c1_out, c2_out, c3_out, 
+    c4_out, c5_out, c6_out, 
+    c7_out, c8_out, c9_out, 
+    done_input_buffer, done_mac, done_q, done_relu);
+
     input clk, reset, start;
     input [215:0] IFM;
     input [215:0] WGT;
     output done;
-    output [47:0] c1_out, c2_out, c3_out; 
-    output [47:0] c4_out, c5_out, c6_out;
-    output [47:0] c7_out, c8_out, c9_out;
-    output done_input_buffer, done_mac;
+    output signed [47:0] c1_out, c2_out, c3_out; 
+    output signed [47:0] c4_out, c5_out, c6_out;
+    output signed [47:0] c7_out, c8_out, c9_out;
+    output done_input_buffer, done_mac, done_q, done_relu;
 
-    wire [23:0] a1, a2, a3;
-    wire [23:0] b1, b2, b3;
+    wire signed [23:0] a1, a2, a3;
+    wire signed [23:0] b1, b2, b3;
 
-    wire [47:0] c1_mac, c2_mac, c3_mac;
-    wire [47:0] c4_mac, c5_mac, c6_mac;
-    wire [47:0] c7_mac, c8_mac, c9_mac;
+    wire signed [47:0] c1_mac, c2_mac, c3_mac;
+    wire signed [47:0] c4_mac, c5_mac, c6_mac;
+    wire signed [47:0] c7_mac, c8_mac, c9_mac;
 
-    systolic_input_buffer input_buffer (
+    wire signed [47:0] q1_mac, q2_mac, q3_mac;
+    wire signed [47:0] q4_mac, q5_mac, q6_mac;
+    wire signed [47:0] q7_mac, q8_mac, q9_mac;
+
+    wire signed [47:0] relu1_mac, relu2_mac, relu3_mac;
+    wire signed [47:0] relu4_mac, relu5_mac, relu6_mac;
+    wire signed [47:0] relu7_mac, relu8_mac, relu9_mac;
+
+    systolic_input_top input_buffer (
         .clk(clk),
         .reset(reset),
         .start(start),
@@ -38,14 +52,40 @@ module NPU (clk, reset, start, IFM, WGT, done, c1_out, c2_out, c3_out, c4_out, c
         .done(done_mac)
     );
 
-    assign c1_out = c1_mac;
-    assign c2_out = c2_mac;
-    assign c3_out = c3_mac;
-    assign c4_out = c4_mac;
-    assign c5_out = c5_mac;
-    assign c6_out = c6_mac;
-    assign c7_out = c7_mac;
-    assign c8_out = c8_mac;
-    assign c9_out = c9_mac;
-    assign done = done_mac;
+    Q16_32_to_Q8_16_pipe cut_bit (
+        .clk(clk),
+        .reset(reset),
+        .enable(done_mac),
+        .din_0(c1_mac), .din_1(c2_mac), .din_2(c3_mac),
+        .din_3(c4_mac), .din_4(c5_mac), .din_5(c6_mac),
+        .din_6(c7_mac), .din_7(c8_mac), .din_8(c9_mac),
+        .dout_0(q1_mac), .dout_1(q2_mac), .dout_2(q3_mac),
+        .dout_3(q4_mac), .dout_4(q5_mac), .dout_5(q6_mac),
+        .dout_6(q7_mac), .dout_7(q8_mac), .dout_8(q9_mac),
+        .done(done_q)
+    );
+
+    ReLU relu (
+        .clk(clk),
+        .reset(reset),
+        .relu_en(done_q),
+        .done(done_relu),
+        .in_0(q1_mac), .in_1(q2_mac), .in_2(q3_mac),
+        .in_3(q4_mac), .in_4(q5_mac), .in_5(q6_mac),
+        .in_6(q7_mac), .in_7(q8_mac), .in_8(q9_mac),
+        .out_0(relu1_mac), .out_1(relu2_mac), .out_2(relu3_mac),
+        .out_3(relu4_mac), .out_4(relu5_mac), .out_5(relu6_mac),
+        .out_6(relu7_mac), .out_7(relu8_mac), .out_8(relu9_mac)
+    );
+
+    assign c1_out = relu1_mac;
+    assign c2_out = relu2_mac;
+    assign c3_out = relu3_mac;
+    assign c4_out = relu4_mac;
+    assign c5_out = relu5_mac;
+    assign c6_out = relu6_mac;
+    assign c7_out = relu7_mac;
+    assign c8_out = relu8_mac;
+    assign c9_out = relu9_mac;
+    assign done = done_relu;
 endmodule
--- testbench/npu_tb.v
+++ testbench/npu_tb.v
@@ -3,9 +3,9 @@ module tb ();
     reg [215:0] IFM, WGT;
     wire done;
     wire done_input_buffer, done_mac;
-    wire [47:0] c1_out, c2_out, c3_out;
-    wire [47:0] c4_out, c5_out, c6_out; 
-    wire [47:0] c7_out, c8_out, c9_out;
+    wire signed [47:0] c1_out, c2_out, c3_out;
+    wire signed [47:0] c4_out, c5_out, c6_out; 
+    wire signed [47:0] c7_out, c8_out, c9_out;
 
     NPU npu (
         .clk(clk),
@@ -27,14 +27,15 @@ module tb ();
         reset = 1;
         start = 0;
         IFM = {
-            24'd1, 24'd2, 24'd3,
-            24'd4, 24'd5, 24'd6,
-            24'd7, 24'd8, 24'd9
+            24'd6554,   24'd13107,  24'd19661,
+            24'd26214,  24'd32768,  24'd39322,
+            24'd45875,  24'd52429,  24'd58982
         };
+
         WGT = {
-            24'd1, 24'd2, 24'd3,
-            24'd4, 24'd5, 24'd6,
-            24'd7, 24'd8, 24'd9
+            24'd6554,   24'd13107,  24'd19661,
+            24'd26214,  24'd32768,  24'd39322,
+            24'd45875,  24'd52429,  24'd58982
         };
         #10;
         reset = 0; 
