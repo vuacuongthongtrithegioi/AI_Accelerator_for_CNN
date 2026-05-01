d2742e32ecb605fa9ac3366ad857e5f4a2fbba0c
williambale7 <nqc1842005@gmail.com>
2026-05-01 13:17:15 +0700

    update MAC Unit

--- testbench/tb_mac.v
+++ testbench/tb_mac.v
@@ -1,19 +1,21 @@
 module tb ();
     reg clk, reset, start;
-    reg [23:0] a1,a2,a3;
-    reg [23:0] b1,b2,b3;
-    wire [48:0] c1,c2,c3;
-    wire [48:0] c4,c5,c6;
-    wire [48:0] c7,c8,c9;
+    reg signed [215:0] IFM;
+    reg signed [215:0] WGT;
+
+    wire signed [47:0] c1,c2,c3;
+    wire signed [47:0] c4,c5,c6;
+    wire signed [47:0] c7,c8,c9;
+
     wire done;
 
-    mac_top dut (
+    mac_unit_top dut (
         .clk(clk),
         .reset(reset),
         .start(start),
 
-        .a1(a1), .a2(a2), .a3(a3),
-        .b1(b1), .b2(b2), .b3(b3),
+        .IFM(IFM),
+        .WGT(WGT),
 
         .c1(c1), .c2(c2), .c3(c3),
         .c4(c4), .c5(c5), .c6(c6),
@@ -28,13 +30,21 @@ module tb ();
         clk = 0;
         reset = 1;
         start = 0;
+        IFM = {
+            24'd1, 24'd2, 24'd3,
+            24'd4, 24'd5, 24'd6,
+            24'd7, 24'd8, 24'd9
+        };
+        WGT = {
+            24'd1, 24'd2, 24'd3,
+            24'd4, 24'd5, 24'd6,
+            24'd7, 24'd8, 24'd9
+        };
         #10;
         reset = 0;
         start = 1;
         #10;
         start = 0;
-        a1 = 24'd1; a2 = 24'd2; a3 = 24'd3;
-        b1 = 24'd1; b2 = 24'd4; b3 = 24'd7;
         #1000;
         $finish;
     end
