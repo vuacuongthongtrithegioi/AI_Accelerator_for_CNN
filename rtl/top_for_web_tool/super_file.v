950b5f17b104875f93bf5a638bafc92b6e1b0064
williambale7 <nqc1842005@gmail.com>
2026-05-09 09:47:00 +0700

    update

--- rtl/top_for_web_tool/super_file.v
+++ rtl/top_for_web_tool/super_file.v
@@ -586,10 +586,269 @@ module mac_reg (
     end
 endmodule
 
+module Q16_32_to_Q8_16_pipe (
+    input  wire clk,
+    input  wire reset,
+    input  wire enable,
+
+  input  wire signed [47:0] din_0, din_1, din_2, din_3, din_4, din_5, din_6, din_7, din_8,
+    output reg  signed [23:0] dout_0, dout_1, dout_2, dout_3, dout_4, dout_5, dout_6, dout_7, dout_8,
+    output reg done
+);
+
+    reg signed [47:0] stage_0, stage_1, stage_2, stage_3, stage_4, stage_5, stage_6, stage_7, stage_8;
+    reg valid1;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            valid1 <= 0;
+        end else begin
+            valid1 <= enable;
+            if (enable) begin
+                stage_0 <= din_0 + 48'sd32768;
+                stage_1 <= din_1 + 48'sd32768;
+                stage_2 <= din_2 + 48'sd32768;
+                stage_3 <= din_3 + 48'sd32768;
+                stage_4 <= din_4 + 48'sd32768;
+                stage_5 <= din_5 + 48'sd32768;
+                stage_6 <= din_6 + 48'sd32768;
+                stage_7 <= din_7 + 48'sd32768;
+                stage_8 <= din_8 + 48'sd32768;
+            end
+        end
+    end
+
+    always @(posedge clk) begin
+        if (reset) begin
+            dout_0 <= 0; dout_1 <= 0; dout_2 <= 0;
+            dout_3 <= 0; dout_4 <= 0; dout_5 <= 0;
+            dout_6 <= 0; dout_7 <= 0; dout_8 <= 0;
+            done <= 0;
+        end else begin
+            done <= valid1;
+
+            if (valid1) begin
+                if (stage_0[47:39] != {9{stage_0[47]}})
+                    dout_0 <= (stage_0[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_0 <= stage_0[39:16];
+                if (stage_1[47:39] != {9{stage_1[47]}})
+                    dout_1 <= (stage_1[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_1 <= stage_1[39:16];
+                if (stage_2[47:39] != {9{stage_2[47]}})
+                    dout_2 <= (stage_2[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_2 <= stage_2[39:16];
+                if (stage_3[47:39] != {9{stage_3[47]}})
+                    dout_3 <= (stage_3[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_3 <= stage_3[39:16];
+                if (stage_4[47:39] != {9{stage_4[47]}})
+                    dout_4 <= (stage_4[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_4 <= stage_4[39:16];
+                if (stage_5[47:39] != {9{stage_5[47]}})
+                    dout_5 <= (stage_5[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_5 <= stage_5[39:16];
+                if (stage_6[47:39] != {9{stage_6[47]}})
+                    dout_6 <= (stage_6[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_6 <= stage_6[39:16];
+                if (stage_7[47:39] != {9{stage_7[47]}})
+                    dout_7 <= (stage_7[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_7 <= stage_7[39:16];
+                if (stage_8[47:39] != {9{stage_8[47]}})
+                    dout_8 <= (stage_8[47]) ? -24'sh800000 : 24'sh7FFFFF;
+                else
+                    dout_8 <= stage_8[39:16];
+            end
+        end
+    end
+
+
+endmodule
+
+
+module ReLU (
+    input clk, reset, relu_en,
+
+    input signed [47:0] in_0, in_1, in_2,
+    input signed [47:0] in_3, in_4, in_5,
+    input signed [47:0] in_6, in_7, in_8,
+
+    output signed [47:0] out_0, out_1, out_2,
+    output signed [47:0] out_3, out_4, out_5,
+    output signed [47:0] out_6, out_7, out_8,
+
+    output reg done
+);
+
+    reg signed [47:0] out_0_reg, out_1_reg, out_2_reg;
+    reg signed [47:0] out_3_reg, out_4_reg, out_5_reg;
+    reg signed [47:0] out_6_reg, out_7_reg, out_8_reg;
+    reg relu_en_d;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            out_0_reg <= 0; out_1_reg <= 0; out_2_reg <= 0;
+            out_3_reg <= 0; out_4_reg <= 0; out_5_reg <= 0;
+            out_6_reg <= 0; out_7_reg <= 0; out_8_reg <= 0;
+        end
+        else if (relu_en) begin
+            out_0_reg <= (in_0 < 0) ? 0 : in_0;
+            out_1_reg <= (in_1 < 0) ? 0 : in_1;
+            out_2_reg <= (in_2 < 0) ? 0 : in_2;
+
+            out_3_reg <= (in_3 < 0) ? 0 : in_3;
+            out_4_reg <= (in_4 < 0) ? 0 : in_4;
+            out_5_reg <= (in_5 < 0) ? 0 : in_5;
+
+            out_6_reg <= (in_6 < 0) ? 0 : in_6;
+            out_7_reg <= (in_7 < 0) ? 0 : in_7;
+            out_8_reg <= (in_8 < 0) ? 0 : in_8;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (reset) begin
+            relu_en_d <= 0;
+            done <= 0;
+        end else begin
+            relu_en_d <= relu_en;
+            done <= relu_en_d; 
+        end
+    end
+
+    assign out_0 = out_0_reg;
+    assign out_1 = out_1_reg;
+    assign out_2 = out_2_reg;
+    assign out_3 = out_3_reg;
+    assign out_4 = out_4_reg;
+    assign out_5 = out_5_reg;
+    assign out_6 = out_6_reg;
+    assign out_7 = out_7_reg;
+    assign out_8 = out_8_reg;
+
+endmodule
+
+module buffer_out (
+    clk, reset, load,
+    c1_in, c2_in, c3_in, 
+    c4_in, c5_in, c6_in, 
+    c7_in, c8_in, c9_in, 
+    c1_out, c2_out, c3_out,
+    c4_out, c5_out, c6_out,
+    c7_out, c8_out, c9_out
+);
+    input clk, reset;
+    input load;
+    input signed [47:0] c1_in, c2_in, c3_in;
+    input signed [47:0] c4_in, c5_in, c6_in;
+    input signed [47:0] c7_in, c8_in, c9_in;
+    output signed [47:0] c1_out, c2_out, c3_out;
+    output signed [47:0] c4_out, c5_out, c6_out;
+    output signed [47:0] c7_out, c8_out, c9_out;
+
+    reg signed [47:0] rC1, rC2, rC3;
+    reg signed [47:0] rC4, rC5, rC6;
+    reg signed [47:0] rC7, rC8, rC9;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            rC1<=0; rC2<=0; rC3<=0;
+            rC4<=0; rC5<=0; rC6<=0;
+            rC7<=0; rC8<=0; rC9<=0;
+        end
+        else if (load) begin
+            rC1<=c1_in; rC2<=c2_in; rC3<=c3_in;
+            rC4<=c4_in; rC5<=c5_in; rC6<=c6_in;
+            rC7<=c7_in; rC8<=c8_in; rC9<=c9_in;
+        end
+    end
+
+    assign c1_out = rC1;
+    assign c2_out = rC2;
+    assign c3_out = rC3;
+    assign c4_out = rC4;
+    assign c5_out = rC5;
+    assign c6_out = rC6;
+    assign c7_out = rC7;
+    assign c8_out = rC8;
+    assign c9_out = rC9;
+
+endmodule
+
+module output_buffer_ctl (clk, reset, done_sytolic, load);
+    input clk, reset, done_sytolic;
+    output reg load;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            load <= 0;
+        end
+        else if (done_sytolic) begin
+            load <= 1;
+        end
+        else begin
+            load <= 0;
+        end
+    end
+endmodule
+
+module output_buffer_top (
+    input clk, reset,
+    input done_systolic,
+
+    input signed [47:0] c1_in, c2_in, c3_in,
+    input signed [47:0] c4_in, c5_in, c6_in,
+    input signed [47:0] c7_in, c8_in, c9_in,
+
+    output signed [47:0] c1_out, c2_out, c3_out,
+    output signed [47:0] c4_out, c5_out, c6_out,
+    output signed [47:0] c7_out, c8_out, c9_out,
+
+    output reg done
+);
+
+    wire load;
+    reg load_d;
+
+    output_buffer_ctl out_ctl (
+        .clk(clk), 
+        .reset(reset),
+        .done_sytolic(done_systolic),
+        .load(load)
+    );
+
+    buffer_out u_buffer_out (
+        .clk(clk), .reset(reset), .load(load),
+        .c1_in(c1_in), .c2_in(c2_in), .c3_in(c3_in),
+        .c4_in(c4_in), .c5_in(c5_in), .c6_in(c6_in),
+        .c7_in(c7_in), .c8_in(c8_in), .c9_in(c9_in),
+        .c1_out(c1_out), .c2_out(c2_out), .c3_out(c3_out),
+        .c4_out(c4_out), .c5_out(c5_out), .c6_out(c6_out),
+        .c7_out(c7_out), .c8_out(c8_out), .c9_out(c9_out)
+    );
+
+    always @(posedge clk) begin
+        if (reset) begin
+            load_d <= 0;
+            done <= 0;
+        end else begin
+            load_d <= load;
+            done <= load_d;   
+        end
+    end 
+endmodule
+
 module NPU (
     clk, reset, start,
     IFM_in, WGT_in,
     done_input_buffer, done_systolic_input, done_mac,
+    done_cut_bit, done_relu, done_output_buffer,
     cout_1, cout_2, cout_3,
     cout_4, cout_5, cout_6,
     cout_7, cout_8, cout_9,
@@ -598,6 +857,7 @@ module NPU (
     input clk, reset, start;
     input signed [215:0] IFM_in, WGT_in;
     output done_input_buffer, done_systolic_input, done_mac;
+    output done_cut_bit, done_relu;
     output signed [47:0] cout_1, cout_2, cout_3;
     output signed [47:0] cout_4, cout_5, cout_6;
     output signed [47:0] cout_7, cout_8, cout_9;
@@ -665,4 +925,51 @@ module NPU (
         .p7_out(pipe7), .p8_out(pipe8), .p9_out(pipe9),
         .valid_out(pipe_valid)
     );
+
+    wire signed [23:0] q1_w, q2_w, q3_w;
+    wire signed [23:0] q4_w, q5_w, q6_w;
+    wire signed [23:0] q7_w, q8_w, q9_w;
+    //Convert the 16.32 fixed-point outputs of the MAC to 8.16 fixed-point format for the ReLU
+    Q16_32_to_Q8_16_pipe cut_bit (
+        .clk(clk),
+        .reset(reset),
+        .enable(pipe_valid), // Enable quantization when registered MAC outputs are valid
+        .din_0(pipe1), .din_1(pipe2), .din_2(pipe3),
+        .din_3(pipe4), .din_4(pipe5), .din_5(pipe6),
+        .din_6(pipe7), .din_7(pipe8), .din_8(pipe9),
+        .dout_0(q1_w), .dout_1(q2_w), .dout_2(q3_w),
+        .dout_3(q4_w), .dout_4(q5_w), .dout_5(q6_w),
+        .dout_6(q7_w), .dout_7(q8_w), .dout_8(q9_w),
+        .done(done_cut_bit) // NPU is done when quantization is done
+    );
+
+    wire signed [23:0] r1_w, r2_w, r3_w;
+    wire signed [23:0] r4_w, r5_w, r6_w;
+    wire signed [23:0] r7_w, r8_w, r9_w;
+    // The ReLU module would be defined elsewhere and would apply ReLU to the quantized outputs when done_cut_bit is high
+    ReLU relu (
+        .clk(clk),
+        .reset(reset),
+        .relu_en(done_cut_bit), // Enable ReLU when quantization is done
+        .in_0(q1_w), .in_1(q2_w), .in_2(q3_w),
+        .in_3(q4_w), .in_4(q5_w), .in_5(q6_w),
+        .in_6(q7_w), .in_7(q8_w), .in_8(q9_w),
+        .out_0(r1_w), .out_1(r2_w), .out_2(r3_w),
+        .out_3(r4_w), .out_4(r5_w), .out_5(r6_w),
+        .out_6(r7_w), .out_7(r8_w), .out_8(r9_w),
+        .done(done_relu) // NPU is done when ReLU is done
+    );
+
+    output_buffer_top output_buffer (
+        .clk(clk),
+        .reset(reset),
+        .start(done_relu), // Start output buffer after ReLU is done
+        .data_in({r1_w, r2_w, r3_w, r4_w, r5_w, r6_w, r7_w, r8_w, r9_w}), // Concatenate ReLU outputs into a single bus
+        .cout_1(cout_1), .cout_2(cout_2), .cout_3(cout_3),
+        .cout_4(cout_4), .cout_5(cout_5), .cout_6(cout_6),
+        .cout_7(cout_7), .cout_8(cout_8), .cout_9(cout_9),
+        .done(done_output_buffer) // NPU is done when output buffer is done
+    );
+
+    assign done_npu = done_output_buffer; // NPU is done 
 endmodule
